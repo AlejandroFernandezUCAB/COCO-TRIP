2507816641939eea85f408f09bd2882f9dbfa708
Unknown <kilordpepo@gmail.com>
2017-10-21 10:45:40 -0400

    Login con conexion a registro

--- FrontOffice/src/pages/login/login.ts
+++ FrontOffice/src/pages/login/login.ts
@@ -27,25 +27,24 @@ export class LoginPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-  continuar(){
-    this.navCtrl.push(HomePage);
-  }
   
-  Login(){
+  login(){
 
-    
+    this.navCtrl.push(HomePage);
   }
   facebookLogin() {
     
     this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
         this.userData = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name']}
+        this.navCtrl.push(HomePage);
       });
     });
   }
 
   googleLogin(){
     this.googleAuth.login();
+    this.navCtrl.push(HomePage);
 
   }
 
