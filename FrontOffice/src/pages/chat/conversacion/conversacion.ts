e9e414cae894339bfb50ff57587792cf52874f11
José Francisco Hernández Balestrini <kikohernandez92@gmail.com>
2017-10-22 18:53:03 -0400

    Ajustes finales UI Conversaciones

--- FrontOffice/src/pages/chat/conversacion/conversacion.ts
+++ FrontOffice/src/pages/chat/conversacion/conversacion.ts
@@ -3,6 +3,7 @@ import { Platform, ActionSheetController } from 'ionic-angular';
 import { IonicPage, NavParams, NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { VisualizarPerfilPage } from '../../VisualizarPerfil/VisualizarPerfil';
+import * as moment from 'moment';
 
 @IonicPage()
 @Component({
@@ -12,7 +13,11 @@ import { VisualizarPerfilPage } from '../../VisualizarPerfil/VisualizarPerfil';
 
 export class ConversacionPage {
 
-    //public lista
+
+  mensajes: Array<msgs> = [
+    {contenido: '¡Adoro este sitio!', tiempo: moment().fromNow() }
+  ];
+
 constructor(public navCtrl: NavController, public navParams: NavParams, public actionsheetCtrl: ActionSheetController, public alertCtrl: AlertController, public platform: Platform) {
 
 //this.lista = navParams.get("let item of listachatRec");
@@ -54,4 +59,75 @@ tapEvent2(){
   alert.present();
 }
 
+pressEvent1(){
+  let actionSheet = this.actionsheetCtrl.create({
+    title: 'Opciones del chat',
+    cssClass: 'action-sheets-basic-page',
+    buttons: [
+      {
+        text: 'Editar mensaje',
+        icon: !this.platform.is('ios') ? 'create' : null,
+        handler: () => {
+          let decision = this.alertCtrl.create({
+            message: '¿Deseas editar este mensaje?',
+            buttons: [
+              {
+                text: 'Sí',
+                handler: () => {
+                  console.log('Decisión de editar afirmativa');
+                }
+              },
+              {
+                text: 'No',
+                handler: () => {
+                  console.log('Decisión de editar negativa');
+                }
+              }
+            ]
+          });
+          decision.present()
+        }
+      },
+      {
+        text: 'Eliminar mensaje',
+        role: 'destructive', // aplica color rojo de alerta
+        icon: !this.platform.is('ios') ? 'trash' : null,
+        handler: () => {
+          let decision = this.alertCtrl.create({
+            message: '¿Borrar este mensaje?',
+            buttons: [
+              {
+                text: 'Sí',
+                handler: () => {
+                  console.log('Decisión de eliminar afirmativa');
+                }
+              },
+              {
+                text: 'No',
+                handler: () => {
+                  console.log('Decisión de eliminar negativa');
+                }
+              }
+            ]
+          });
+          decision.present()
+        }
+      },
+      {
+        text: 'Cancelar',
+        role: 'cancel', //coloca el botón siempre en el último lugar.
+        icon: !this.platform.is('ios') ? 'close' : null,
+        handler: () => {
+          console.log('Cancelar ActionSheet');
+        }
+      }
+    ]
+  });
+  actionSheet.present();
+  }
+}
+
+interface msgs{
+  contenido: string;
+  tiempo: string;
 }
