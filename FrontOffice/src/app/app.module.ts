7b180a5e66e760c33e5b74e8504215b0b81917d3
Aquilesal <aquiles0505@hotmail.com>
2017-10-30 10:32:30 -0400

    arreglando errores

--- FrontOffice/src/app/app.module.ts
+++ FrontOffice/src/app/app.module.ts
@@ -49,9 +49,8 @@ import { VisualizarPerfilPublicoPage } from '../pages/visualizarperfilpublico/vi
 
 import { CrearGrupoPage } from '../pages/crear-grupo/crear-grupo';
 
-import { CrearGrupo2Page } from '../pages/crear-grupo2/crear-grupo2';
+import { SeleccionarIntegrantesPage } from '../pages/seleccionar-integrantes/seleccionar-integrantes';
 import { DetalleGrupoPage } from '../pages/detalle-grupo/detalle-grupo';
-import { AgregarAmigoPage } from '../pages/agregar-amigo/agregar-amigo';
 import { CalendarModule } from "ion2-calendar";
 import { EventosCalendarioService } from '../services/eventoscalendario'
 import { BuscarAmigoPage } from '../pages/buscar-amigo/buscar-amigo';
@@ -74,7 +73,7 @@ import { BuscarAmigoPage } from '../pages/buscar-amigo/buscar-amigo';
     VisualizarPerfilPage,
     VisualizarPerfilPublicoPage,
     CrearGrupoPage,
-    CrearGrupo2Page,
+    SeleccionarIntegrantesPage,
     DetalleGrupoPage,
     BuscarAmigoPage,
     RegisterPage,
@@ -111,7 +110,7 @@ import { BuscarAmigoPage } from '../pages/buscar-amigo/buscar-amigo';
     VisualizarPerfilPage,
     VisualizarPerfilPublicoPage,
     CrearGrupoPage,
-    CrearGrupo2Page,
+    SeleccionarIntegrantesPage,
     DetalleGrupoPage,
     BuscarAmigoPage,
     RegisterPage,
