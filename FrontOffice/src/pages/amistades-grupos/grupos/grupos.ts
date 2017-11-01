b21f11507e499fb0964b48426df79134ac70fff1
Aquilesal <aquiles0505@hotmail.com>
2017-11-01 19:13:55 -0400

    agregando boton

--- FrontOffice/src/pages/amistades-grupos/grupos/grupos.ts
+++ FrontOffice/src/pages/amistades-grupos/grupos/grupos.ts
@@ -8,11 +8,15 @@ import{DetalleGrupoPage} from '../../detalle-grupo/detalle-grupo';
   templateUrl: 'grupos.html'
 })
 export class GruposPage {
-  
+  delete= false;
+  edit= false;
+  detail=false;
   Grupo=[];
+  its= Array();
+  noIts = false;
   
     constructor(public navCtrl: NavController, public platform: Platform,
-      public actionsheetCtrl: ActionSheetController,public alerCtrl: AlertController) {
+      public actionsheetCtrl: ActionSheetController,public alertCtrl: AlertController) {
       
   
       
@@ -20,13 +24,13 @@ export class GruposPage {
     [
      {
  
-         img: 'https://pbs.twimg.com/media/DLty-BkUIAE9LRJ.jpg', nombre: 'Informatica'
+         id:1, img: 'https://pbs.twimg.com/media/DLty-BkUIAE9LRJ.jpg', nombre: 'Informatica'
  
      },
  
      {
  
-         img: 'https://pbs.twimg.com/media/DL9S8ZhUIAE_jZt.jpg', nombre: 'Desarrollo'
+         id:2, img: 'https://pbs.twimg.com/media/DL9S8ZhUIAE_jZt.jpg', nombre: 'Desarrollo'
  
      }
  
@@ -36,12 +40,95 @@ export class GruposPage {
   
    
 
-  CrearGrupo(){
+  crearGrupo(){
+    this.edit=false;
+    this.detail=false;
+    this.delete=false;
 
     this.navCtrl.push(CrearGrupoPage);
   }
 
- 
+  eliminar(){
+    this.edit=false;
+    this.detail=false;
+
+    if (this.delete==false){
+
+      this.delete = true;
+    }
+    else{
+      this.delete=false;
+    }
+    
+  }
+
+  editar(){
+    this.delete=false;
+    this.detail=false;
+
+    if (this.edit==false){
+
+      this.edit = true;
+    }
+    else{
+      this.edit=false;
+    }
+    
+  }
+
+
+  detallegrupo(){
+    this.delete=false;
+    this.edit=false;
+    if(this.detail==false){
+
+      this.detail = true;
+    }
+    else{
+
+      this.detail=false;
+    }
+    
+  }
+
+  
+
+  eliminarGrupo(id, index) {
+    const alert = this.alertCtrl.create({
+    title: 'Por favor, confirmar',
+    message: '¿Desea borrar este Grupo?',
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          //console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Aceptar',
+        handler: () => {
+          //this.eliminarItinerario(id, index);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  verDetalleGrupo() {
+    this.navCtrl.push(DetalleGrupoPage);
+  
+  }
+
+  eliminarItinerario(id, index){
+    let eliminado = this.its.filter(item => item.id === id)[0];
+    var removed_elements = this.its.splice(index, 1);
+    if (this.its.length == 0){
+      this.noIts = true;
+      console.log("no its")
+    }
+  }
   
 
   pressEvent() {
@@ -68,7 +155,7 @@ export class GruposPage {
           role: 'destructive',
           icon: !this.platform.is('ios') ? 'trash' : null,
           handler: () => {
-            let confirm = this.alerCtrl.create({
+            let confirm = this.alertCtrl.create({
               title: 'Eliminar Grupo',
               message: '¿Seguro que deseas eliminar a este grupo?',
               buttons: [
