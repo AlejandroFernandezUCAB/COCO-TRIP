8981329e0dde8fe308fbbc82f7ad7443c7449271
MichelGJ <michjraich12@hotmail.com>
2017-10-31 21:09:55 -0400

    creacion controlador M5

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M5_Controller.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRest_COCO_TRIP.Controllers
+{
+  public class M5_Controller : ApiController
+  {
+    
+  }
+}
