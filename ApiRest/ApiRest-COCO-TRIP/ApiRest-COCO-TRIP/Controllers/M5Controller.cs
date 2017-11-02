5dc9cb8d1a4103aad863e97ec7cce560185b4385
MichelGJ <michjraich12@hotmail.com>
2017-11-02 18:14:39 -0400

    agregadas librerias NUnit y Npgsql en el directorio y actualizadas las referencias

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M5Controller.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M5Controller.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using Npgsql;
 using System.Data;
 using ApiRest_COCO_TRIP.Models.M5;
+using ApiRest_COCO_TRIP.Models;
 namespace ApiRest_COCO_TRIP.Controllers
 {
   public class M5Controller : ApiController
@@ -15,10 +16,9 @@ namespace ApiRest_COCO_TRIP.Controllers
     {
       try
       {
-        stringcon = "Server=127.0.0.1;User Id=postgres; " + "Password=20977974pg;Database=cocotrip;";
-        conn = new NpgsqlConnection(stringcon);
-        conn.Open();
-        NpgsqlCommand comm = new NpgsqlCommand("add_itinerario", conn);
+        ConexionBase con = new ConexionBase();
+        con.Conectar();
+        NpgsqlCommand comm = new NpgsqlCommand("add_itinerario", con.SqlConexion);
         comm.CommandType = CommandType.StoredProcedure;
         comm.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, it.It_nombre);
         comm.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Date, it.It_fechaInicio);
@@ -27,7 +27,7 @@ namespace ApiRest_COCO_TRIP.Controllers
         NpgsqlDataReader pgread = comm.ExecuteReader();
         pgread.Read();
         Boolean resp = pgread.GetBoolean(0);
-        conn.Close();
+        con.Desconectar();
         return resp;
       }
       catch (NpgsqlException e)
