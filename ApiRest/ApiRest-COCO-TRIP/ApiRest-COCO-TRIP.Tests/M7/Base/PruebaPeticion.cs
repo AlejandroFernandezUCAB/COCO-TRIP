8bbdcb6864313229ee4820621d0442842d3da374
Joaquin Camacho <ssrmatu@gmail.com>
2017-11-02 23:24:18 -0400

    Agregado prueba unitaria TestInsertarLugarTuristico de la clase Peticion. Modulo 7

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP.Tests/M7/Base/PruebaPeticion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP.Tests/M7/Base/PruebaPeticion.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using ApiRest_COCO_TRIP.Models.M7.Base;
 using ApiRest_COCO_TRIP.Models.M7.Dato;
+using System;
 
 namespace ApiRest_COCO_TRIP.Tests.M7.Base
 {
@@ -11,10 +12,10 @@ namespace ApiRest_COCO_TRIP.Tests.M7.Base
   public class PruebaPeticion
   {
     private Peticion peticion;
-    /*private LugarTuristico lugar;
+    private LugarTuristico lugar;
     private Actividad actividad;
     private Horario horario;
-    private Foto foto;*/
+    private Foto foto;
 
     /// <summary>
     /// Instancia los objetos
@@ -24,7 +25,51 @@ namespace ApiRest_COCO_TRIP.Tests.M7.Base
     {
       peticion = new Peticion();
 
-      //Continuo mañana
+      lugar = new LugarTuristico();
+      lugar.Id = 2;
+      lugar.Nombre = "Parque Generalisimo de Miranda";
+      lugar.Costo = 0;
+      lugar.Descripcion = "Lugar al aire libre";
+      lugar.Direccion = "Frente a la estacion Miranda, Caracas";
+      lugar.Correo = "parquemiranda@gob.ve";
+      lugar.Telefono = 02122732867;
+      lugar.Latitud = 20.0;
+      lugar.Longitud = 10.0;
+      lugar.Activar = true;
+
+      byte[] imagen = new byte[28480];
+
+      actividad = new Actividad();
+      actividad.Id = 2;
+      actividad.Nombre = "Parque Generalisimo de Miranda";
+      actividad.Duracion = new TimeSpan(2, 0, 0);
+      actividad.Descripcion = "Lugar al aire libre";
+      actividad.Foto.Contenido = imagen;
+      actividad.Activar = true;
+
+      horario = new Horario();
+      horario.Id = 2;
+      horario.DiaSemana = (int)Horario.Dia.Domingo;
+      horario.HoraApertura = new TimeSpan(8, 0, 0);
+      horario.HoraCierre = new TimeSpan(17, 0, 0);
+
+      foto = new Foto();
+      foto.Id = 2;
+      foto.Contenido = imagen;
+
+      lugar.Actividad.Add(actividad);
+      lugar.Horario.Add(horario);
+      lugar.Foto.Add(foto);
+    }
+
+    /// <summary>
+    /// Test del metodo InsertarLugarTuristico de la clase Peticion
+    /// </summary>
+    [Test]
+    [Category("Peticion.Insertar")]
+    public void TestInsertarLugarTuristico()
+    {
+      Assert.AreEqual(lugar.Id, peticion.InsertarLugarTuristico(lugar));
     }
 
   }
