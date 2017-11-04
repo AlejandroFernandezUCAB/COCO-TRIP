447b53b8f09d8960671c4b8c30b2780a11e3333a
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-11-04 12:00:29 -0400

    Se agregó el método comentado de agregar preferencia y eliminar preferencia.

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Models/M1/Usuario.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Models/M1/Usuario.cs
@@ -9,6 +9,7 @@ namespace ApiRest_COCO_TRIP.Models.M1
   {
     private string nombreUsuario;
     private string correo;
+    //private List<Categoria> preferencias;
 
     public Usuario()
     {
@@ -16,5 +17,20 @@ namespace ApiRest_COCO_TRIP.Models.M1
 
     public string Correo { get => correo; set => correo = value; }
     public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+    //public List<Categoria> Preferencias { get => preferencias ; set => preferencias = value; }
+
+    /*
+    public List<Categoria> agregarPreferencia ( Categoria nuevaCategoria ){
+      
+      preferencias.add( nuevaCategoria );
+    }
+     */
+
+    /*
+
+    public List<Categoria> eliminarPreferencia(){
+
+    }
+     */
   }
 }
