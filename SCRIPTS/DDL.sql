f1782c8f2e2363ca8ae734203ee43555b978452d
Carlos Valero <kilordpepo@gmail.com>
2017-11-04 18:55:52 -0400

    Cambio en tabla usuario

--- SCRIPTS/DDL.sql
+++ SCRIPTS/DDL.sql
@@ -5,16 +5,16 @@ CREATE DATABASE cocotrip WITH OWNER = admin_cocotrip ENCODING = UTF8;
 --Modulo 1
 CREATE TABLE USUARIO (
     us_id		integer,
-    us_nombreUsuario 	varchar(20),
+    us_nombreUsuario 	varchar(20) NOT NULL UNIQUE,
     us_nombre         	varchar(30) NOT NULL,
     us_apellido        	varchar(30) NOT NULL,
     us_fechaNacimiento  date NOT NULL,
     us_genero		varchar(1) CHECK (us_genero ='M' OR us_genero='F'),
-    us_email	        varchar(30),
+    us_email	        varchar(30) NOT NULL UNIQUE,
     us_password         varchar(20),
     us_foto		bytea,
     us_validacion	boolean,
-    CONSTRAINT primaria_usuario PRIMARY KEY(us_id,us_nombreUsuario,us_email)
+    CONSTRAINT primaria_usuario PRIMARY KEY(us_id)
     
 );
 --Fin de modulo 
