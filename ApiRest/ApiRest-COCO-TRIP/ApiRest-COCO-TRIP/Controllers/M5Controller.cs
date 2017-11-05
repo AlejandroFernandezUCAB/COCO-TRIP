774623825f221f74913e505535f2f9e4e58db361
MichelGJ <michjraich12@hotmail.com>
2017-11-04 21:29:38 -0400

    ajuste http

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M5Controller.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M5Controller.cs
@@ -48,7 +48,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// </summary>
     /// <param name="it">el itinerario a eliminar</param>
     /// <returns>true si elimina existosamente, false en caso de error</returns>
-    [HttpGet]
+    [HttpDelete]
     public Boolean EliminarItinerario(Itinerario it)
     {
       try
@@ -76,7 +76,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// </summary>
     /// <param name="it">el itinerario a modificar</param>
     /// <returns>true si modifica existosamente, false en caso de error</returns>
-    [HttpGet]
+    [HttpPost]
     public Boolean ModificarItinerario(Itinerario it)
     {
       try
@@ -138,7 +138,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// <param name="it">itinerario al cual se le agrega la actividad</param>
     /// <param name="ac">actividad a agregar en el itinerario</param>
     /// <returns>true si se agrego la actividad exitosamente, false en caso de error</returns>
-   [HttpGet]
+   [HttpPost]
     public Boolean AgregarActividad_It(Itinerario it, Actividad ac)
     {
       try
@@ -168,7 +168,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// <param name="it">itinerario al cual se le agrega el lugar turistico</param>
     /// <param name="lt">lugar turistico a agregar en el itinerario</param>
     /// <returns>true si se agrego el lugar turistico exitosamente, false en caso de error</returns>
-    [HttpGet]
+    [HttpPost]
     public Boolean AgregarLugar_It(Itinerario it, LugarTuristico lt)
     {
       try
@@ -228,7 +228,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// <param name="it">itinerario del cual se elimina la actividad</param>
     /// <param name="ac">actividad a eliminar del itinerario</param>
     /// <returns>true si se elimino la actividad exitosamente, false en caso de error</returns>
-    [HttpGet]
+    [HttpDelete]
     public Boolean EliminarActividad_It(Itinerario it, Actividad ac)
     {
       try
@@ -258,7 +258,7 @@ namespace ApiRest_COCO_TRIP.Controllers
     /// <param name="it">itinerario del cual se elimina el lugar turistico</param>
     /// <param name="lt">lugar turistico a eliminar del itinerario</param>
     /// <returns>true si se elimino el lugar turistico exitosamente, false en caso de error</returns>
-    [HttpGet]
+    [HttpDelete]
     public Boolean EliminarLugar_It(Itinerario it, LugarTuristico lt)
     {
       try
