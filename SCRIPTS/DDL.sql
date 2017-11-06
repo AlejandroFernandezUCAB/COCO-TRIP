1e95436dd61095629c5bb22c20b608b76d601475
noe <noehrrr@gmail.com>
2017-11-05 23:53:49 -0400

    scripts modulo 8

--- SCRIPTS/DDL.sql
+++ SCRIPTS/DDL.sql
@@ -135,6 +135,45 @@ CREATE TABLE LT_Foto
 );
 --Fin de modulo 
 --Modulo 8
+create table evento
+
+(
+
+	ev_id int primary key,
+	ev_nombre varchar(100) not null,
+
+	ev_descripcion varchar(500),
+
+	ev_precio int,
+
+	ev_fecha_inicio timestamp,
+
+	ev_fecha_fin timestamp,
+
+	ev_hora_inicio time,
+
+	ev_hora_fin time,
+
+	ev_foto bytea,
+
+	ev_localidad int,
+
+	ev_categoria int
+
+);
+
+
+create table localidad
+(
+	lo_id int primary key,
+
+	lo_nombre varchar(200),
+
+	lo_descripcion varchar(500),
+
+	lo_lugar varchar (500)
+
+);
 --Fin de modulo 
 --Modulo 9
 CREATE TABLE categoria 
@@ -164,6 +203,9 @@ CREATE TABLE categoria
 --Modulo 7
 --Fin de modulo 
 --Modulo 8
+alter table evento add constraint fk_categoria foreign key (ev_categoria) references categoria (ca_id);
+
+alter table evento add constraint fk_localidad foreign key (ev_localidad) references localidad (lo_id);
 --Fin de modulo 
 --Modulo 9
 
@@ -235,6 +277,28 @@ CREATE INDEX IX_LT_HORARIO ON lt_horario (fk_ho_lugar_turistico, ho_id);
 CREATE INDEX IX_LT_FOTO ON lt_foto (fk_fo_lugar_turistico, fo_id);
 --Fin de modulo 
 --Modulo 8
+CREATE SEQUENCE SEQ_Evento
+
+	START WITH 1
+
+	INCREMENT BY 1
+
+	NO MINVALUE
+
+	NO MAXVALUE
+
+	CACHE 1;
+
+
+CREATE SEQUENCE SEQ_Localidad
+
+	START WITH 1
+
+	INCREMENT BY 1
+
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
 --Fin de modulo 
 --Modulo 9
 --Fin de modulo 
