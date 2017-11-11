22f158aa095b6ec6340a4f2cee07a07460752d2b
Unknown <rn2702@gmail.com>
2017-11-10 21:32:48 -0400

    Controller: CambiarPassword, BorrarUsuario

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M2_EditarDatosController.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M2_EditarDatosController.cs
@@ -46,6 +46,71 @@ namespace ApiRest_COCO_TRIP.Controllers
 
     }
 
+    /// <summary>
+    /// Metodo Post para actualizar la contraseña del usuario del usuario. hará dos llamdas a base de datos, una para buscar id de usuario
+    /// y otra para modificar la contraseña con los parametros recibidos
+    /// </summary>
+    /// <param name="idUsuario">Id del usuario </param>
+    /// <param name="username">Username del usuario</param>
+    /// <param name="password">Contraseña del usuario </param>
+    /// <returns>bool</returns>
+    public bool CambiarPass(string username, string password)
+    {
+      peticion = new PeticionPerfil();
+      idUsuario = peticion.ConsultarIdDelUsuario(username);
+      try
+      {
+        if (idUsuario == 1)
+        {
+          peticion.CambiarPassword(idUsuario,password);
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      catch (NpgsqlException e)
+      {
+        return false;
+      }
+
+      
+    }
+
+    /// <summary>
+    /// Metodo Post para borrar al usuario. hará dos llamdas a base de datos, una para buscar id de usuario
+    /// y otra para borrar al usuario con el parametro usado
+    /// </summary>
+    /// <param name="idUsuario">Id del usuario </param>
+    /// <param name="username">Username del usuario</param>
+    /// <param name="password">Contraseña del usuario </param>
+    /// <returns>bool</returns>
+
+    public bool BorrarUsuario (string username, string password)
+    {
+      peticion = new PeticionPerfil();
+      idUsuario = peticion.ConsultarIdDelUsuario(username);
+      try
+      {
+        if (idUsuario == 1)
+        {
+          peticion.BorrarUsuario(idUsuario, password);
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      catch
+      {
+        return false;
+      }
+    }
+
 
   }
+
+
 }
