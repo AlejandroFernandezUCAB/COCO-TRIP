4157a5684a157a512eb8d749c037361c65a61a02
j0rg3md <marin10jorge@gmail.com>
2017-11-10 23:55:25 -0400

    Creacion de controlador y Clase de peticion para Eventos

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Models/PeticionEvento.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiRest_COCO_TRIP.Models
+{
+  public class PeticionEvento
+  {
+    internal static bool AgregarEvento(Evento evento)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal static bool EliminarEvento(int v, int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal static Evento ConsultarEvento(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal static bool EliminarEvento(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal static List<Evento> ListaEventosPorCategoria(int id_categoria)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
