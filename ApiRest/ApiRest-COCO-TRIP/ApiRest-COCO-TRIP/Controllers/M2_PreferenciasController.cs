9b76ecc11efd5fbb231fc231a2eff2620ed9cc67
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-11-11 11:41:38 -0400

    Cambio en el archivo de peticion y en los scripts

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M2_PreferenciasController.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Controllers/M2_PreferenciasController.cs
@@ -2,33 +2,32 @@ using Npgsql;
 using System.Web.Http;
 using ApiRest_COCO_TRIP.Models;
 using System.Collections.Generic;
+using System.Web.Http.Cors;
+
 namespace ApiRest_COCO_TRIP.Controllers
 {
+  [EnableCors(origins: "*", headers: "*", methods: "*")]
   public class M2_PreferenciasController : ApiController
   {
 
-    private PeticionPerfil peticion;
+    protected PeticionPerfil peticion;
 
     /// <summary>
     /// Metodo Post para agregar una preferencia del usuario, hará una llamda a base de datos para buscar id de usuario
     /// y id de categoria para agregarlo en la tabla de preferencias
     /// </summary>
-    /// <param name="id">Id de categoria</param>
-    /// <param name="nombre">Nombre de la categoria</param>
-    /// <param name="descripcion">Descripcion de la categoria</param>
-    /// <param name="estatus">Estatus de la categoria</param>
-    /// <param name="nivel">Nivel de la categoria</param>
     /// <param name="nombreUsuario">Nombre del usuario</param>
-    /// <returns>Lista de categorias del usuario</returns>
+    /// <param name="nombrePreferencia">Nombre de la preferencia a agregar</param>
+    /// <returns>Lista de preferencias del usuario</returns>
     // GET api/<controller>/<action>/prefencia
     [HttpPost]
-    public List<Categoria> AgregarPreferencia( int id, string nombre, string descripcion,
-                                                bool estatus, int nivel, string nombreUsuario)
+    public List<Categoria> AgregarPreferencias ( string nombreUsuario , string nombrePreferencia)
     {
 
       Usuario usuario = new Usuario();
       List<Categoria> preferencias = new List<Categoria>();
-      Categoria categoria = new Categoria( id, nombre, descripcion, estatus, nivel);
+      Categoria categoria = new Categoria();
+      PeticionPerfil peticion;
 
       usuario.NombreUsuario = nombreUsuario;
       usuario.Preferencias = preferencias;
@@ -42,10 +41,14 @@ namespace ApiRest_COCO_TRIP.Controllers
         peticion = new PeticionPerfil();
         //Busco el nombre de usuario
         usuario.Id = peticion.ConsultarIdDelUsuario( usuario.NombreUsuario );
+
         if (usuario.Id == -1) 
         {
+
           return null; //No hay usuario en la bdd con -1
+
         }
+
         else //Se agrega al array de objetos y se busca el id en la BDD.
         {
 
