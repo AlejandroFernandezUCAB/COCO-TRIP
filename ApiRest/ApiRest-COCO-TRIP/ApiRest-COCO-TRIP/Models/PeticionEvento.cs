f96092b5a44c2c86ef954d081135b0aa8e4c2515
miguel2m <miguelangelmedina2494@mail.com>
2017-11-12 14:48:17 -0400

    Modificando Agregar Evento
    Modificando Agregar Evento

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Models/PeticionEvento.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Models/PeticionEvento.cs
@@ -1,9 +1,11 @@
+using ApiRest_COCO_TRIP.Models.Excepcion;
 using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Reflection;
 
 namespace ApiRest_COCO_TRIP.Models
 {
@@ -15,29 +17,53 @@ namespace ApiRest_COCO_TRIP.Models
 
     public PeticionEvento()
     {
-      conexion.Conectar();
-    }
-
-    public  bool AgregarEvento(Evento evento)
+            try
+            {
+                conexion = new ConexionBase();
+                conexion.Conectar();
+            }
+            catch (BaseDeDatosExcepcion e)
+            {
+                e.NombreMetodos.Add(this.GetType().FullName + "." + MethodBase.GetCurrentMethod().Name);
+                throw e;
+            }
+        }
+      /// <summary>
+      /// Metodo que agrega eventos y retorna ecenario de exito y fallo
+      /// </summary>
+      /// <param name="evento"> Objeto del tipo Evento</param>
+      /// <returns> Respuesta de Agregar con exito</returns>
+    public  int AgregarEvento(Evento evento)
     {
-      
-        comando = new NpgsqlCommand("Add_evento", conexion.SqlConexion);
-        comando.CommandType = CommandType.StoredProcedure;
-        //Aqui registro los valores
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Nombre);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Descripcion);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Double, evento.Precio);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Date, evento.FechaInicio);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Date, evento.FechaFin);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Timestamp, evento.HoraInicio);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Timestamp, evento.HoraFin);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Foto);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Integer, evento.IdCategoria);
-        comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Integer, evento.IdLocalidad);
-        read = comando.ExecuteReader();
-        read.Read();
-      //Logica para retornar hablar con noe
-      return false;
+            int respuesta = -1;
+            try
+            {
+
+                comando = new NpgsqlCommand("InsertarEvento", conexion.SqlConexion);
+                comando.CommandType = CommandType.StoredProcedure;
+                //Aqui registro los valores
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Nombre);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Descripcion);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Double, evento.Precio);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Date, evento.FechaInicio);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Date, evento.FechaFin);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Timestamp, evento.HoraInicio);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Timestamp, evento.HoraFin);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Varchar, evento.Foto);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Integer, evento.IdCategoria);
+                comando.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Integer, evento.IdLocalidad);
+                read = comando.ExecuteReader();
+                read.Read();
+                respuesta = read.GetInt32(0);
+                conexion.Desconectar();
+            }
+            catch (BaseDeDatosExcepcion e)
+            {
+                e.NombreMetodos.Add(this.GetType().FullName + "." + MethodBase.GetCurrentMethod().Name);
+                throw e;
+            }
+            return respuesta;
+           
     }
 
     internal static bool EliminarEvento(int v, int id)
@@ -59,6 +85,5 @@ namespace ApiRest_COCO_TRIP.Models
     {
       throw new NotImplementedException();
     }
-
   }
 }
