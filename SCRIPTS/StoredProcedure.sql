1a22d6bcc9243e037f9d697f254ab1d87550f263
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-11-12 11:41:25 -0400

    Stored  procedure de buscarlistapreferenciasporcategoria

--- SCRIPTS/StoredProcedure.sql
+++ SCRIPTS/StoredProcedure.sql
@@ -235,7 +235,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION BuscarListaPreferenciaUsuario
+CREATE OR REPLACE FUNCTION BuscarListaPreferenciasPorCategoria
 ( _idUsuario int, _nombrePreferencia varchar)
 RETURNS TABLE( 
   id int,
@@ -245,11 +245,11 @@ BEGIN
   RETURN QUERY SELECT
 	c.ca_id, c.ca_nombre
 	FROM categoria c
-	WHERE ca_id NOT IN (Select c.ca_id from preferencia p where p.pr_usuario = _idUsuario and p.pr_categoria = c.ca_id );
+	WHERE ca_id NOT IN (Select c.ca_id from preferencia p where p.pr_usuario = _idUsuario and p.pr_categoria = c.ca_id ) 
+	AND LOWER(c.ca_nombre) LIKE CONCAT(LOWER(_nombrePreferencia),'%');
 END;
-
 $$ LANGUAGE plpgsql;
-
+SELECT BuscarListaPreferenciasPorCategoria(1,'T')
 CREATE OR REPLACE FUNCTION ModificarDatosUsuario
 ( _idUsuario int , _nombre varchar , _apellido varchar , _fechaNacimiento date , _genero varchar ) 
 RETURNS integer AS $$
