d556f7b1ac3c3e3b3b1ecec8c76e07a7d49464a2
Joaquin Camacho <ssrmatu@gmail.com>
2017-11-13 19:43:58 -0400

    Arreglado conflictos del BACKOFFICE

--- BackOffice/BackOffice-COCO-TRIP/Models/Peticion/PeticionLugares.cs
+++ BackOffice/BackOffice-COCO-TRIP/Models/Peticion/PeticionLugares.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using BackOffice_COCO_TRIP.Models.Dato;
+using System.Collections.Generic;
 
 namespace BackOffice_COCO_TRIP.Models.Peticion
 {
@@ -23,10 +24,10 @@ namespace BackOffice_COCO_TRIP.Models.Peticion
 
     //GET
 
-    public List<LugarTuristico> GetLista (int desde, int hasta)
+    /*public List<LugarTuristico> GetLista (int desde, int hasta)
     {
 
-    }
+    }*/
 
   }
 }
