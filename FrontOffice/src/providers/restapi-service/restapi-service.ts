ec293b26be9faf17f7b1c8c09af3f306971d9807
Carlos Valero <kilordpepo@gmail.com>
2017-11-13 18:32:18 -0400

    arreglando

--- FrontOffice/src/providers/restapi-service/restapi-service.ts
+++ FrontOffice/src/providers/restapi-service/restapi-service.ts
@@ -56,6 +56,7 @@ export class RestapiService {
   registrarse(nombreUsuario,correo,nombre,apellido,genero,fechaNacimiento,clave,foto) 
   {   
       this.userData={nombreUsuario : nombreUsuario,correo: correo,nombre: nombre,apellido: apellido,genero: genero,fechaNacimiento: fechaNacimiento, clave : clave,foto: ""};
+      console.log('Enviando: '+JSON.stringify(this.userData));
       return new Promise(resolve => {
       this.http.post(this.apiUrl+'/M1_Login/registrarusuario/?datos='+JSON.stringify(this.userData),"")
       .map(res => res.json())
@@ -63,6 +64,7 @@ export class RestapiService {
         this.data = data;
         resolve(this.data);
       },error=>{
+        console.log('ERROR: '+error);
         resolve(-1);
 
       });
