b1ab71527928402b1b21fb51cfe78f213f8d3699
MichelGJ <michjraich12@hotmail.com>
2017-12-14 19:07:31 -0400

    creada entidad(entity) categoria

---
+++ BackOffice/BackOffice-COCO-TRIP/Datos/Entidades/Categoria.cs
@@ -0,0 +1,67 @@
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackOffice_COCO_TRIP.Datos.Entidades
+{
+  public class Categoria:Entidad
+  {
+    public Categoria() { }
+
+    public Categoria(int Id)
+    {
+      this.Id = Id;
+    }
+
+    private string _name;
+
+    [JsonProperty(PropertyName = "nombre")]
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "Debe llenar este campo")]
+    public string Name
+    {
+      get { return _name; }
+      set { _name = value; }
+    }
+
+
+    private string _description;
+
+    [JsonProperty(PropertyName = "descripcion")]
+    [Display(Name = "Descripcion")]
+    [Required(ErrorMessage = "Debe llenar este campo")]
+    public string Description
+    {
+      get { return _description; }
+      set { _description = value; }
+    }
+
+    [JsonProperty(PropertyName = "estatus")]
+    private bool _status;
+
+    public bool Status
+    {
+      get { return _status; }
+      set { _status = value; }
+    }
+
+    private int _categories;
+
+    [JsonProperty(PropertyName = "categoriaSuperior")]
+    [Required]
+    public int UpperCategories
+    {
+      get { return _categories; }
+      set { _categories = value; }
+    }
+
+    private int nivel;
+
+    [JsonProperty(PropertyName = "nivel")]
+    public int Nivel
+    {
+      get { return nivel; }
+      set { nivel = value; }
+    }
+
+  }
+}
--- BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoModificarCategoria.cs
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoModificarCategoria.cs
@@ -1,4 +1,6 @@
 using System;
+using BackOffice_COCO_TRIP.Models.Peticion;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +9,9 @@ namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
 {
   public class ComandoModificarCategoria : Comando
   {
+    PeticionCategoria peti = new PeticionCategoria();
     public override void Execute()
     {
-
     }
     public override Object GetResult()
     {
