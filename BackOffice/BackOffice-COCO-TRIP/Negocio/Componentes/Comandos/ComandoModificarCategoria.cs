af9e2f1d37146d836bc45ab2cc6d7f74523c7583
MichelGJ <michjraich12@hotmail.com>
2017-12-14 18:34:07 -0400

    comando modificar categoria

---
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoModificarCategoria.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
+{
+  public class ComandoModificarCategoria : Comando
+  {
+    public override void Execute()
+    {
+
+    }
+    public override Object GetResult()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
