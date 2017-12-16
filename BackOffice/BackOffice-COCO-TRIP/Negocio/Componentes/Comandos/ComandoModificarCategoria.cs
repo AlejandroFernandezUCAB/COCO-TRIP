b09f1537b807dc99d0d3c33bbc4bf0292e87968c
MichelGJ <michjraich12@hotmail.com>
2017-12-16 19:43:50 -0400

    agregado DAOCategoria y su respectivo metodo en FabricaDao, creado ComandoModificarCategoria y la entidad Categoria

--- BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoModificarCategoria.cs
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoModificarCategoria.cs
@@ -1,21 +1,53 @@
-using System;
-using BackOffice_COCO_TRIP.Models.Peticion;
+using BackOffice_COCO_TRIP.Datos.Entidades;
 using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
+using System.Collections;
+using BackOffice_COCO_TRIP.Negocio.Fabrica;
+using BackOffice_COCO_TRIP.Datos.DAO;
 
 namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
 {
   public class ComandoModificarCategoria : Comando
   {
-    PeticionCategoria peti = new PeticionCategoria();
+    private Categoria categoria;
+    DAO<JObject, Categoria> dao = FabricaDAO.GetDAOCategoria();
+    
     public override void Execute()
     {
+      try
+      {
+        DAO<JObject, Categoria> dao = FabricaDAO.GetDAOCategoria();
+        DAOCategoria daoc = (DAOCategoria)dao;
+        JObject respuesta = daoc.PutEditarCategoria(categoria);
+/*       if (respuesta.Property("data") != null)
+        {
+          resultado.Add(respuesta["data"].ToObject<List<Categories>>());
+          resultado.Add("Exito");
+        }
+
+        else
+        {
+          resultado.Add(new List<Categories>());
+          resultado.Add("Ocurrio un error durante la comunicacion, revise su conexion a internet");
+        }*/
+      }
+      catch (Exception e)
+      {
+        //TERMINAR
+        throw e;
+      }
+
+
     }
-    public override Object GetResult()
+    public override ArrayList GetResult()
     {
       throw new NotImplementedException();
+    }
+   
+
+    public override void SetPropiedad(object propiedad)
+    {
+      categoria = (Categoria)propiedad;
     }
   }
 }
