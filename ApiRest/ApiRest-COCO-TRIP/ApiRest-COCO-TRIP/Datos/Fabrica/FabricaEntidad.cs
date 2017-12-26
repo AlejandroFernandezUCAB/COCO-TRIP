b32f4c93ad417359d610a3f404d923a869036412
Carlos Valero <kilordpepo@gmail.com>
2017-12-26 01:30:53 -0400

    Merge branch 'Modulo_8' into origin/Developer

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Datos/Fabrica/FabricaEntidad.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Datos/Fabrica/FabricaEntidad.cs
@@ -1,4 +1,91 @@
+using ApiRest_COCO_TRIP.Datos.Entity;
 
+namespace ApiRest_COCO_TRIP.Datos.Fabrica
+{
+  /// <summary>
+  /// Fabrica que instancia las entidades
+  /// </summary>
+  public class FabricaEntidad
+  {
+    /// <summary>
+    /// Retorna la instancia de la entidad Grupo
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Grupo CrearEntidadGrupo()
+    {
+      return new Grupo();
+    }
+
+    /// <summary>
+    /// Retorna la instancia de la entidad Usuario
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Usuario CrearEntidadUsuario()
+    {
+      return new Usuario();
+    }
+
+    /// <summary>
+    /// Retorna la instancia de la entidad Foto
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Foto CrearEntidadFoto()
+    {
+      return new Foto();
+    }
+
+    /// <summary>
+    /// Retorna la instancia de la entidad Amigo
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Amigo CrearEntidadAmigo()
+    {
+      return new Amigo();
+    }
+
+    /// <summary>
+    /// Retorna la instancia de la entidad Categoria
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Categoria CrearEntidadCategoria()
+    {
+      return new Categoria();
+    }
+
+    /// Retorna la instancia de la entidad Itinerario
+    /// </summary>
+    /// <returns>Itinerario</returns>
+    public static Itinerario CrearEntidadItinerario()
+    {
+      return new Itinerario();
+    }
+
+    /// <summary>
+    /// Retorna la instancia de la entidad Agenda
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Agenda CrearEntidadAgenda()
+    {
+      return new Agenda();
+    }
+
+    /// <summary>
+    /// Retorna una nueva instancia de la entidad LocalidadEvento
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static LocalidadEvento CrearEntidadLocalidad()
+    {
+      return new LocalidadEvento();
+    }
+
+    /// <summary>
+    /// Retorna una nueva instancia de la entidad Evento
+    /// </summary>
+    /// <returns>Grupo</returns>
+    public static Evento CrearEntidadEvento()
+    {
+      return new Evento();
+    }
     /// Retorna una nueva instancia de la entidad LocalidadEvento
     /// Retorna la instancia de la entidad Notificacion
     /// </summary>
@@ -7,5 +94,6 @@
     {
       return new Notificacion();
     }
-    /// <summary>
-    /// Retorna la instancia de la entidad LocalidadEvento
+
+  }
+}
