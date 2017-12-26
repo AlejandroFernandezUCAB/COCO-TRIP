e71a3059c9b4dac01224f724a76800021a24796c
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-26 19:19:07 -0400

    Using faltantes en las excepciones

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/WebExcepcion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/WebExcepcion.cs
@@ -1,7 +1,9 @@
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 
 namespace ApiRest_COCO_TRIP.Comun.Excepcion
 {
