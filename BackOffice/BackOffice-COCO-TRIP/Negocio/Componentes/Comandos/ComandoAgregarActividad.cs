f3936101ebe021cd779040670a2b4bf755c5e9e8
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-26 21:53:37 -0400

    Quitando librerias sin uso en nuestros pocos comandos

--- BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoAgregarActividad.cs
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoAgregarActividad.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 
 namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
 {
