42c2517b2ef53cb662bd598c4700d5b89e7a69b8
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-26 21:48:28 -0400

    Agregando los comandos con sus clases abstractas respectivamente y modificando un problema del .csproj que no aparecian las calses que habia creado

--- BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoConsultarLugaresTuristicos.cs
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoConsultarLugaresTuristicos.cs
@@ -1,11 +1,26 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
 {
-  public class ComandoConsultarLugaresTuristicos
+  public class ComandoConsultarLugaresTuristicos : Comando
   {
+    public override void Execute()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override ArrayList GetResult()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void SetPropiedad(object propiedad)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
