37d467a505fb73d5519076a1be2f28599468eeed
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-26 20:27:49 -0400

    Agregando el DAO con sus interfaces respectivamente, solo en estructura, aun no hay codigo implementado

---
+++ BackOffice/BackOffice-COCO-TRIP/Datos/DAO/DAOLugar_Turistico.cs
@@ -0,0 +1,34 @@
+using System;
+using BackOffice_COCO_TRIP.Datos.Entidades;
+using Newtonsoft.Json.Linq;
+
+namespace BackOffice_COCO_TRIP.Datos.DAO
+{
+  public class DAOLugar_Turistico : DAO<JObject, Entidad>
+  {
+    public override JObject Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override JObject Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override JObject Patch(Entidad data)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override JObject Post(Entidad data)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override JObject Put(Entidad data)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
