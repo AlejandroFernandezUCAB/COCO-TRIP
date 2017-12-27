4d18a2edd2b9eca48eb0d99c9f7e5a5b208d50c2
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-26 21:36:24 -0400

    Agregando los comandos basicos para agregar lugares turisticos

---
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Componentes/Comandos/ComandoAgregarLugarTuristico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOffice_COCO_TRIP.Negocio.Componentes.Comandos
+{
+  public class ComandoAgregarLugarTuristico
+  {
+  }
+}
