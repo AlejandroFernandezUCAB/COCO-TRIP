0e4f20e47b3a4ee6915b3147f2eae3334a7979d6
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-12-27 23:02:46 -0400

    Agregados DAO de entidades nuevas

---
+++ BackOffice/BackOffice-COCO-TRIP/Datos/DAO/DAOFoto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BackOffice_COCO_TRIP.Datos.Entidades;
+
+namespace BackOffice_COCO_TRIP.Datos.DAO
+{
+  public class DAOFoto
+  {
+  }
+}
