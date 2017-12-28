5434db94189ed2084f83936e8c7285b0f8bce7f0
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-12-28 16:13:33 -0400

    implementando DAO

--- BackOffice/BackOffice-COCO-TRIP/Controllers/M7_LugaresTuristicosController.cs
+++ BackOffice/BackOffice-COCO-TRIP/Controllers/M7_LugaresTuristicosController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BackOffice_COCO_TRIP.Datos.DAO;
+using BackOffice_COCO_TRIP.Datos.Entidades;
 
 namespace BackOffice_COCO_TRIP.Controllers
 {
   public class M7_LugaresTuristicosController
   {
+    public M7_LugaresTuristicosController()
+    {
+      
+    }
   }
 }
--- BackOffice/BackOffice-COCO-TRIP/Datos/DAO/DAOLugar_Turistico.cs
+++ BackOffice/BackOffice-COCO-TRIP/Datos/DAO/DAOLugar_Turistico.cs
@@ -1,19 +1,113 @@
 using System;
 using BackOffice_COCO_TRIP.Datos.Entidades;
 using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json;
 
 namespace BackOffice_COCO_TRIP.Datos.DAO
 {
   public class DAOLugar_Turistico : DAO<JObject, Entidad>
   {
+    private const string ControllerUri = "M7_LugaresTuristicosController";
+    private JObject responseData;
+
     public override JObject Delete(int id)
     {
       throw new NotImplementedException();
     }
 
-    public override JObject Get(int id)
+    /// <summary>
+    /// Metodo Get para obtener los lugares turisticos
+    /// </summary>
+    /// <param name="cantidad"></param>
+    /// <returns>JObject</returns>
+    public override JObject Get(int cantidad)
     {
-      throw new NotImplementedException();
+      try
+      {
+        using (HttpClient cliente = new HttpClient())
+        {
+          cliente.BaseAddress = new Uri(BaseUri);
+          cliente.DefaultRequestHeaders.Accept.Clear();
+          //necesito un metodo del api rest que me traiga las fotos de un lugar.
+          // 'ConsultarFotos' aun no existe.
+          // store procedure que me ayudara: ConsultarFotos
+
+          var responseTask = cliente.GetAsync($"{BaseUri}/{ControllerUri}/GetLista?desde=1&hasta={cantidad}");
+          responseTask.Wait();
+          var response = responseTask.Result;
+          var readTask = response.Content.ReadAsAsync<JObject>();
+          readTask.Wait();
+          responseData = readTask.Result;
+        }
+      }
+      catch (HttpRequestException ex)
+      {
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+
+      catch (WebException ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+      catch (SocketException ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+      catch (AggregateException ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+      catch (JsonSerializationException ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+      catch (JsonReaderException ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", ex.Message }
+
+          };
+      }
+      catch (Exception ex)
+      {
+
+        responseData = new JObject
+          {
+            { "error", $"Ocurrio un error inesperado: {ex.Message}" }
+
+          };
+      }
+
+      return responseData;
     }
 
     public override JObject Patch(Entidad data)
