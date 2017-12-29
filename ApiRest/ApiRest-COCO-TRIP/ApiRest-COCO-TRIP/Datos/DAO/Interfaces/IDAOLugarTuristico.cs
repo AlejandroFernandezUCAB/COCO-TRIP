0531b884e3458e4d1983aa26c6328b6a5b2e1a8d
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-28 23:45:05 -0400

    Creando el comando de LT y su IDAO

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Datos/DAO/Interfaces/IDAOLugarTuristico.cs
@@ -0,0 +1,22 @@
+﻿using ApiRest_COCO_TRIP.Datos.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest_COCO_TRIP.Datos.DAO.Interfaces
+{
+	interface IDAOLugarTuristico
+	{
+
+		void Actualizar(Entidad objeto);
+
+		List<Entidad> ConsultarLista(Entidad objeto);
+
+		Entidad ConsultarPorId(Entidad objeto);
+		void Eliminar(Entidad objeto);
+
+		void Insertar(Entidad objeto);
+	}
+}
