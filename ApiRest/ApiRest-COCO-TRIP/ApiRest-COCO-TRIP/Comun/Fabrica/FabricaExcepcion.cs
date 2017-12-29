85d8a1294b5844471287273755bb83d5ca64dfcd
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-28 21:03:03 -0400

    Creada la fabrica de excepciones, ahora si

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Fabrica/FabricaExcepcion.cs
@@ -0,0 +1,15 @@
+using System;
+using ApiRest_COCO_TRIP.Datos.Entity;
+using ApiRest_COCO_TRIP.Negocio.Command;
+
+namespace ApiRest_COCO_TRIP.Comun.Fabrica
+{
+  /// <summary>
+  /// Fabrica que instancia las Excepciones.
+  /// </summary>
+  public static class FabricaExcepcion
+  {
+    
+
+  }
+}
