da1db5b7f54a9e57de858a97d176f6c8c997c975
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-28 22:57:53 -0400

    Agregando nuevos DAO

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Datos/DAO/DAOHorario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using ApiRest_COCO_TRIP.Datos.Entity;
+
+namespace ApiRest_COCO_TRIP.Datos.DAO
+{
+	public class DAOHorario : DAO
+	{
+		public override void Actualizar(Entidad objeto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override List<Entidad> ConsultarLista(Entidad objeto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Entidad ConsultarPorId(Entidad objeto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Eliminar(Entidad objeto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Insertar(Entidad objeto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
