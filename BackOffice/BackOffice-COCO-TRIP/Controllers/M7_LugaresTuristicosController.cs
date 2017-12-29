5a110dd21b3ec78d7556135214336cfacfa605b8
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2017-12-28 21:18:40 -0400

    Agregando el primer metodo del create, aun no se modificar la ruta del controller del view

--- BackOffice/BackOffice-COCO-TRIP/Controllers/M7_LugaresTuristicosController.cs
+++ BackOffice/BackOffice-COCO-TRIP/Controllers/M7_LugaresTuristicosController.cs
@@ -1,17 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using BackOffice_COCO_TRIP.Datos.DAO;
-using BackOffice_COCO_TRIP.Datos.Entidades;
+using System.Web.Mvc;
+using BackOffice_COCO_TRIP.Negocio.Componentes.Comandos;
+using BackOffice_COCO_TRIP.Negocio.Fabrica;
 
 namespace BackOffice_COCO_TRIP.Controllers
 {
-  public class M7_LugaresTuristicosController
+  public class M7_LugaresTuristicosController : Controller
   {
     public M7_LugaresTuristicosController()
     {
       
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.Title = "Localidades de los Eventos";
+      Comando comando = FabricaComando.GetComandoAgregarLugarTuristico();
+      comando.Execute();
+      TempData["listaLocalidadEvento"] = comando.GetResult();
+      return View(comando.GetResult()[0]);
+    }
+
   }
 }
