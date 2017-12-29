b2e62d539f1cf29bf02940bf37f5eee8b4be7f87
Gianfranco Verrocchi <gianfrancoverrocchi@gmail.com>
2017-12-29 00:19:03 -0400

    Agregada interfaz IDAOFoto

---
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Datos/DAO/Interfaces/IDAOFoto.cs
@@ -0,0 +1,17 @@
+﻿using ApiRest_COCO_TRIP.Datos.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest_COCO_TRIP.Datos.DAO.Interfaces
+{
+    interface IDAOFoto
+    {
+
+        void Eliminar(Entidad objeto);
+
+        void Insertar(Entidad objeto);
+    }
+}
