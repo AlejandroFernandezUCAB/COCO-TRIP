d952f7256af9d9e33094989395d6dea372f6421d
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-28 21:10:47 -0400

    Primer metodo de la fabrica Excepcion creada.

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Fabrica/FabricaExcepcion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Fabrica/FabricaExcepcion.cs
@@ -1,15 +1,17 @@
 using System;
-using ApiRest_COCO_TRIP.Datos.Entity;
-using ApiRest_COCO_TRIP.Negocio.Command;
+using ApiRest_COCO_TRIP.Comun.Excepcion;
 
 namespace ApiRest_COCO_TRIP.Comun.Fabrica
 {
-  /// <summary>
-  /// Fabrica que instancia las Excepciones.
-  /// </summary>
-  public static class FabricaExcepcion
-  {
-    
+    /// <summary>
+    /// Fabrica que instancia las Excepciones.
+    /// </summary>
+    public static class FabricaExcepcion
+    {
+        public static AgregadoExcepcion CrearAgregadoExcepcion(AggregateException excepcion, string nombreMetodo, string mensaje)
+        {
+            return new AgregadoExcepcion(excepcion, nombreMetodo, mensaje);
+        }
 
-  }
+    }
 }
