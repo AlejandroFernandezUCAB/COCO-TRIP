b0c9228533f31a488192898158c504723bab2995
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-30 10:14:32 -0400

    Cambio en el namespace

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/ParametrosNullException.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/ParametrosNullException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ApiRest_COCO_TRIP.Models.Excepcion
+namespace ApiRest_COCO_TRIP.Comun.Excepcion
 {
   public class ParametrosNullException : Exception
   {
