c9e670ad556170ed639f13463e3a7500bf044773
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-30 13:27:36 -0400

    Quitando librerias de WebExcepcion

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/WebExcepcion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/WebExcepcion.cs
@@ -1,9 +1,5 @@
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 using System;
 using System.Net;
-using System.Net.Http;
-using System.Net.Sockets;
 
 namespace ApiRest_COCO_TRIP.Comun.Excepcion
 {
