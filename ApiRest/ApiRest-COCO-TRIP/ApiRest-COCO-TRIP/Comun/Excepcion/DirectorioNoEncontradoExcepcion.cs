3901239aadaa7bec711494eb1a3883a1ca650925
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-30 06:51:03 -0400

    Documentacion agregada de 'DirectorioNoEncontradoExcepcion'

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/DirectorioNoEncontradoExcepcion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/DirectorioNoEncontradoExcepcion.cs
@@ -1,43 +1,50 @@
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Sockets;
 using System.IO;
 
 namespace ApiRest_COCO_TRIP.Comun.Excepcion
 {
     /// <summary>
-    /// Clase que encapsula la información de "DirectoryNotFoundException"
-    /// Breve descripcion de cuando se genera.
+    /// Excepcion logica que encapsula la información de "DirectoryNotFoundException"
+    /// Se produce cuando no se encuentra parte de un archivo o directorio.
     /// </summary>
     public class DirectorioNoEncontradoExcepcion: DirectoryNotFoundException
     {
         private DirectoryNotFoundException excepcion;
         private DateTime fechaHora;   //Hora y fecha de cuando se genero la excepción.
-        private string nombreMetodo;  //Nombre del metodo donde se genero la excepción
+        private string nombreMetodo;  //Nombre del metodo donde se genero la excepción.
         private string mensaje;       //Breve descripción de la excepción genereda con parametro del metodo con la que se ocasiono.
 
         /// <summary>
-        /// Getters y Setters
+        /// Getters y Setters del atributo "excepcion".
         /// </summary>
         public DirectoryNotFoundException Excepcion { get => excepcion; set => excepcion = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "fechaHora". 
+        /// </summary>
         public DateTime FechaHora { get => fechaHora; set => fechaHora = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "nombreMetodo".
+        /// </summary>
         public string NombreMetodo { get => nombreMetodo; set => nombreMetodo = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "mensaje".
+        /// </summary>
         public string Mensaje { get => mensaje; set => mensaje = value; }
 
         /// <summary>
-        /// Constructor
+        /// Metodo Constructor.
         /// </summary>
-        /// <param name="excepcion">Excepción generada del tipo "DirectoryNotFoundException"</param>
+        /// <param name="excepcion">Excepción generada del tipo "DirectoryNotFoundException".</param>
         public DirectorioNoEncontradoExcepcion(DirectoryNotFoundException excepcion) {
             this.excepcion = excepcion;
             this.fechaHora = DateTime.Now;
         }
 
         /// <summary>
-        /// Constructor
+        /// Metodo Constructor
         /// </summary>
         /// <param name="excepcion">Excepción generada del tipo "DirectoryNotFoundException"</param>
         /// <param name="nombreMetodo">Nombre del metodo donde se genero la excepción</param>
