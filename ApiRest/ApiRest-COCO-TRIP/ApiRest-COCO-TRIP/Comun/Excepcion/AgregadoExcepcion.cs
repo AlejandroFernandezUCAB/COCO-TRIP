0f8190dbc3b3060188cf1a25bc929f9d44ef8f38
Horacio Orrillo <horacio.orrillo@gmail.com>
2017-12-30 07:08:16 -0400

    Documentacion de 'AgregadoExcepcion' arreglada

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/AgregadoExcepcion.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Comun/Excepcion/AgregadoExcepcion.cs
@@ -9,7 +9,7 @@ namespace ApiRest_COCO_TRIP.Comun.Excepcion
 {
     /// <summary>
     /// Clase que encapsula la información de "AggregateException"
-    /// Breve descripcion de cuando se genera.
+    /// representa uno o mas errores que se producen durante la ejecucion del programa.
     /// </summary>
     public class AgregadoExcepcion: AggregateException
     {
@@ -19,15 +19,27 @@ namespace ApiRest_COCO_TRIP.Comun.Excepcion
         private string mensaje;       //Breve descripción de la excepción genereda con parametro del metodo con la que se ocasiono.
 
         /// <summary>
-        /// Getters y Setters
+        /// Getters y Setters del atributo "excepcion".
         /// </summary>
         public AggregateException Excepcion { get => excepcion; set => excepcion = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "fechaHora".
+        /// </summary>
         public DateTime FechaHora { get => fechaHora; set => fechaHora = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "nombreMetodo".
+        /// </summary>
         public string NombreMetodo { get => nombreMetodo; set => nombreMetodo = value; }
+
+        /// <summary>
+        /// Getters y Setters del atributo "mensaje".
+        /// </summary>
         public string Mensaje { get => mensaje; set => mensaje = value; }
 
         /// <summary>
-        /// Constructor
+        /// Metodo Constructor.
         /// </summary>
         /// <param name="excepcion">Excepción generada del tipo "AggregateException"</param>
         public AgregadoExcepcion(AggregateException excepcion) {
@@ -36,19 +48,7 @@ namespace ApiRest_COCO_TRIP.Comun.Excepcion
         }
 
         /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="excepcion">Excepción generada del tipo "AggregateException"</param>
-        /// <param name="mensaje">Breve mensaje referenciando como se genero la excepcion, incluir parametros del metodo</param>
-        public AgregadoExcepcion(AggregateException excepcion, string mensaje)
-        {
-            this.excepcion = excepcion;
-            this.mensaje = mensaje;
-            this.fechaHora = DateTime.Now;
-        }
-
-        /// <summary>
-        /// Constructor
+        /// Metodo Constructor.
         /// </summary>
         /// <param name="excepcion">Excepción generada del tipo "AggregateException"</param>
         /// <param name="nombreMetodo">Nombre del metodo donde se genero la excepción</param>
@@ -58,7 +58,7 @@ namespace ApiRest_COCO_TRIP.Comun.Excepcion
             this.excepcion = excepcion;
             this.nombreMetodo = nombreMetodo;
             this.mensaje = mensaje;
-            this.fechaHora = DateTime.Now;
+            fechaHora = DateTime.Now;
         }
     }
 }
