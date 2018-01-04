084ad6756b79e4349cd60342d099bb0b36e875cc
Unknown <gianfrancoverrocchi@gmail.com>
2018-01-03 20:41:12 -0400

    Agregando atributos, constructor y using a comando eliminar foto

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoLTEliminarFoto.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoLTEliminarFoto.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using ApiRest_COCO_TRIP.Datos.Entity;
+using ApiRest_COCO_TRIP.Datos.Fabrica;
+using ApiRest_COCO_TRIP.Datos.DAO.Interfaces;
+using ApiRest_COCO_TRIP.Comun.Excepcion;
+using NLog;
 
 namespace ApiRest_COCO_TRIP.Negocio.Command
 {
     public class ComandoLTEliminarFoto : Comando
     {
+        private IDAOFoto iDAOFoto;
+        private Entidad _foto;
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public ComandoLTEliminarFoto(Entidad foto){
+            iDAOFoto = FabricaDAO.CrearDAOFoto();
+        }
+
         public override void Ejecutar()
         {
             throw new NotImplementedException();
