9191e3b9f54e216b572040f38ce8a89532d1fc32
Unknown <gianfrancoverrocchi@gmail.com>
2018-01-03 20:46:18 -0400

    Implementado ejecutar de comando eliminar fotos del lugar turistico

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoLTEliminarFoto.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoLTEliminarFoto.cs
@@ -12,15 +12,53 @@ namespace ApiRest_COCO_TRIP.Negocio.Command
     {
         private IDAOFoto iDAOFoto;
         private Entidad _foto;
+        private Entidad _lugarTuristico;
         private static Logger log = LogManager.GetCurrentClassLogger();
 
-        public ComandoLTEliminarFoto(Entidad foto){
+        public ComandoLTEliminarFoto(Entidad foto, Entidad lugarTuristico){
             iDAOFoto = FabricaDAO.CrearDAOFoto();
+            _lugarTuristico = lugarTuristico;
         }
 
         public override void Ejecutar()
         {
-            throw new NotImplementedException();
+            try
+            {
+                iDAOFoto.Eliminar(_foto , _lugarTuristico);
+            }
+            catch (ReferenciaNulaExcepcion e)
+			{
+				log.Error(e.Mensaje);
+				throw new ReferenciaNulaExcepcion(e, "Parametros de entrada nulos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+			}
+			catch (CasteoInvalidoExcepcion e)
+			{
+
+				log.Error("Casteo invalido en:"
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new CasteoInvalidoExcepcion(e, "Casteo invalido en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+
+			}
+			catch (BaseDeDatosExcepcion e)
+			{
+
+				log.Error("Ocurrio un error en la base de datos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new BaseDeDatosExcepcion(e, "Ocurrio un error en la base de datos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+
+			}
+			catch (Excepcion e)
+			{
+
+				log.Error("Ocurrio un error desconocido: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new Excepcion(e, "Ocurrio un error desconocido en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+
+			}
         }
 
         public override Entidad Retornar()
