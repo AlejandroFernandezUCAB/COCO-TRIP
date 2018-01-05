c0b8a3aeba0fba7753493ab7cc906dbe62df5a10
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-01-04 23:09:41 -0400

    Agregando nuevo comando para obtener informacion basica de los lugares turisticos

--- ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoObtenerLugaresTuristicos.cs
+++ ApiRest/ApiRest-COCO-TRIP/ApiRest-COCO-TRIP/Negocio/Command/ComandoObtenerLugaresTuristicos.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using ApiRest_COCO_TRIP.Datos.Entity;
 using ApiRest_COCO_TRIP.Datos.Fabrica;
 using ApiRest_COCO_TRIP.Datos.DAO.Interfaces;
+using ApiRest_COCO_TRIP.Comun.Excepcion;
+using System.Reflection;
+using NLog;
 
 namespace ApiRest_COCO_TRIP.Negocio.Command
 {
@@ -10,15 +13,46 @@ namespace ApiRest_COCO_TRIP.Negocio.Command
 	{
 		IDAOLugarTuristico _dao = FabricaDAO.CrearDAOLugarTuristico();
 		List<Entidad> _lugaresTuristicos = new List<Entidad>();
+		private static Logger log = LogManager.GetCurrentClassLogger();
 
 		public override void Ejecutar()
 		{
 			try
 			{
 				_lugaresTuristicos = _dao.ConsultarTodaLaLista();
+				log.Info("Lugares Turisticos:"+ _lugaresTuristicos);
 			}
-			catch()
+			catch (ReferenciaNulaExcepcion e)
 			{
+				log.Error(e.Mensaje);
+				throw new ReferenciaNulaExcepcion(e, "Parametros de entrada nulos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+			}
+			catch (CasteoInvalidoExcepcion e)
+			{
+
+				log.Error("Casteo invalido en:"
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new CasteoInvalidoExcepcion(e, "Casteo invalido en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+
+			}
+			catch (BaseDeDatosExcepcion e)
+			{
+
+				log.Error("Ocurrio un error en la base de datos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new BaseDeDatosExcepcion(e, "Ocurrio un error en la base de datos en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+
+			}
+			catch (Excepcion e)
+			{
+
+				log.Error("Ocurrio un error desconocido: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
+				throw new Excepcion(e, "Ocurrio un error desconocido en: "
+				+ GetType().FullName + "." + MethodBase.GetCurrentMethod().Name + ". " + e.Message);
 
 			}
 		}
