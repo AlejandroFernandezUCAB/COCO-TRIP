6dc67e48a7b4a5b5e9b340debddb47505de456ce
Carlos <kilordpepo@gmail.com>
2018-01-06 00:27:26 -0400

    comentarios DAO y Entidad

--- BackOffice/BackOffice-COCO-TRIP/Negocio/Fabrica/FabricaDAO.cs
+++ BackOffice/BackOffice-COCO-TRIP/Negocio/Fabrica/FabricaDAO.cs
@@ -4,13 +4,25 @@ using Newtonsoft.Json.Linq;
 
 namespace BackOffice_COCO_TRIP.Negocio.Fabrica
 {
+  /// <summary>
+  /// Clase que representa la fabrica de DAO, aqui se deben retornar todos los comandos del sistema.
+  /// </summary>
   public static class FabricaDAO
   {
+    /// <summary>
+    /// Metodo que devuelve el DAOEvento
+    /// </summary>
+    /// <returns>DAOEvento</returns>
     public static DAOEvento GetDAOEvento()
     {
 
       return new DAOEvento();
     }
+
+    /// <summary>
+    /// Metodo que devuelve el DAOLocalidad
+    /// </summary>
+    /// <returns>DAOLocalidad</returns>
     public static DAOLocalidad GetDAOLocalidad()
     {
 
